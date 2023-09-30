61fd1d0b8e3772e7a040f4ec2ea97c02d39aba64
Chiamaka <chiamakabrowneyes@gmail.com>
2023-09-30 14:17:18 -0700

    Initial Commit

---
+++ AnyaMessenger/AnyaMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnyaMessengerApp.swift
+//  AnyaMessenger
+//
+//  Created by chiamakabrowneyes on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct AnyaMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
